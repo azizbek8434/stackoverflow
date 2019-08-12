229fd2796f9aa7025e51d160d2d7050fdc33ed8e
azizbek8434 <azizbek_muzaffarov@mail.ru>
2019-08-12 22:00:16 +0500

    prepoplate users & questions tables using model factories & seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\User;
+use App\Question;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,5 +14,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        factory(User::class, 3)->create()->each(function ($user) {
+            $user->questions()
+                ->saveMany(
+                    factory(Question::class, rand(3, 5))->make()
+                );
+        });
     }
 }
