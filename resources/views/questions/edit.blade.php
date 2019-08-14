66f3d8b8ea50b9b76a767c5f58e358b27ff3764d
azizbek8434 <azizbek_muzaffarov@mail.ru>
2019-08-14 12:43:04 +0500

    updating the question part-2

--- resources/views/questions/edit.blade.php
+++ resources/views/questions/edit.blade.php
@@ -8,7 +8,7 @@
             <div class="card">
                 <div class="card-header">
                     <div class="d-flex align-items-center">
-                        <h2>Ask Question</h2>
+                        <h2>Edit Question</h2>
                         <div class="ml-auto">
                             <a href="{{ route('questions.index') }}" class="btn btn-outline-secondary">
                                 Back to all questions
@@ -17,9 +17,10 @@
                     </div>
                 </div>
                 <div class="card-body">
-                    <form method="post" action="{{ route('questions.store') }}">
+                    <form method="post" action="{{ route('questions.update', $question->id) }}">
+                        {{ method_field('PUT') }}
                         @include('questions._form', [
-                            'buttonText' =>  'Ask Questions'
+                        'buttonText' => 'Update Questions'
                         ])
                     </form>
                 </div>
--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -7,50 +7,57 @@
         <div class="col-md-12">
             <div class="card">
                 <div class="card-header">
-                <div class="d-flex align-items-center">
-                    <h2>All Questions</h2>
-                    <div class="ml-auto">
-                        <a href="{{ route('questions.create') }}" class="btn btn-outline-secondary">
-                            Ask Question
-                        </a>
+                    <div class="d-flex align-items-center">
+                        <h2>All Questions</h2>
+                        <div class="ml-auto">
+                            <a href="{{ route('questions.create') }}" class="btn btn-outline-secondary">
+                                Ask Question
+                            </a>
+                        </div>
                     </div>
-                </div>
                 </div>
                 @include('layouts._message')
                 <div class="card-body">
-                
-                @foreach($questions as $question)
+
+                    @foreach($questions as $question)
                     <div class="media">
-                    <div class="d-flex flex-column counters">
-                        <div class="vote">
-                            <strong>{{ $question->votes }}</strong> {{ str_plural('vote', $question->votes) }}
+                        <div class="d-flex flex-column counters">
+                            <div class="vote">
+                                <strong>{{ $question->votes }}</strong> {{ str_plural('vote', $question->votes) }}
+                            </div>
+                            <div class="status {{ $question->status }}">
+                                <strong>{{ $question->answers }}</strong> {{ str_plural('answer', $question->answers) }}
+                            </div>
+                            <div class="view">
+                                {{ $question->views ." ". str_plural('view', $question->views) }}
+                            </div>
                         </div>
-                        <div class="status {{ $question->status }}">
-                            <strong>{{ $question->answers }}</strong> {{ str_plural('answer', $question->answers) }}
-                        </div>
-                        <div class="view">
-                            {{ $question->views ." ". str_plural('view', $question->views) }}
-                        </div>
-                    </div>
                         <div class="media-body">
-                            <h3 class="mt-0">
-                                <a href="{{ $question->url }}" >{{ $question->title }}</a>
-                            </h3>
+                            <div class="d-flex line-items-center">
+                                <h3 class="mt-0">
+                                    <a href="{{ $question->url }}">{{ $question->title }}</a>
+                                </h3>
+                                <div class="ml-auto">
+                                    <a href="{{ route('questions.edit', $question->id) }}"
+                                        class="btn btn-outline-info btn-sm">Edit</a>
+                                </div>
+                            </div>
+
                             <p class="lead">
-                                Asked by 
-                                <a href="{{ $question->user->url }}" >{{ $question->user->name }}</a>
+                                Asked by
+                                <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
                                 <span class="text-muted">
-                                {{ $question->created_date }}
+                                    {{ $question->created_date }}
                                 </span>
                             </p>
                             {{ str_limit($question->body, 250) }}
                         </div>
                     </div>
                     <hr>
-                @endforeach
+                    @endforeach
                 </div>
                 <div class="card-footer">
-                {{ $questions->links() }}
+                    {{ $questions->links() }}
                 </div>
             </div>
         </div>
